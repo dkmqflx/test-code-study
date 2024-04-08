f8a32a1361dfecead6ec6cf862c42d71b7782094
dkmqflx <dkmqflx@gmail.com>
2024-04-08 23:33:30 +0900

    docs: 2.1 단위 테스트란 무엇일까?

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -5,10 +5,30 @@ import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
 it('className prop으로 설정한 css class가 적용된다.', async () => {
+  /**
+   * 모든 테스트 코드는 디스크립션을 작성해야 한다
+   * 테스트의 가독성도 굉장히 중요하기 때문에 디스크립션을 명확하게 적어야 한다
+   *
+   * Arrange - 테스트를 위한 환경 만들기
+   * -> className을 지닌 컴포넌트 렌더링
+   *
+   * Act - 테스트할 동작 발생
+   * -> 렌더링에 대한 검증이기 때문에 현재 이 단계는 생략
+   * -> 클릭이나 메서드 호출, prop 변경 등등에 대한 작업이 여기에 해당
+   *
+   * Assert - 올바른 동작이 실행되었는지 검증
+   * -> 렌더링 후 DOM에 해당 classs가 존재하는지 검증
+   */
+
+  // render API 호출 -> 테스트 환경의 jsDOM에 리액트 컴포넌트가 렌더링된 DOM 구조가 반영
+  // jsDOM: Node.js에서 사용하기 위한 많은 웹 표준을 순수 자바스크립트로 구현
   await render(<TextField className="my-class" />);
 
   const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
 
+  // vitest의 expect 함수를 사용하여 기대 결과를 검증
+  // className이란 내부 prop이나 state 값을 검증 (X)
+  // 렌더링되는 DOM 구조가 올바르게 변경되었는지 확인 (O) -> 최종적으로 사용자가 보는 결과는 DOM
   expect(textInput).toHaveClass('my-class');
 });
 
