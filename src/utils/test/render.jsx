6d5b872a05bb93fc113d99d6ffcf5a593e91a6c1
dkmqflx <dkmqflx@gmail.com>
2024-04-13 16:17:59 +0900

    4.5. msw로 API 모킹하기

--- src/utils/test/render.jsx
+++ src/utils/test/render.jsx
@@ -5,11 +5,14 @@ import React from 'react';
 import { Toaster } from 'react-hot-toast';
 import { MemoryRouter } from 'react-router-dom';
 
+// 테스트 환경 설정 파일
 export default async (component, options = {}) => {
   const { routerProps } = options;
   const user = userEvent.setup();
 
   // https://tanstack.com/query/v4/docs/react/guides/testing
+  // logs 필드는 defaultOptions와 같은 계층의 logger 필드로 변경되었음
+  // 공식문서 참조할 것
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
